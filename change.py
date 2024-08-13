40bf5dd6a2c293ae6cb9bc1e362df4d4a875a018
Biancamelli <biancamelli249@gmail.com>
2024-08-13 20:16:16 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,12 @@
-def change():
-    expense = 23.75
-    money = 100
+def cambio():
+    gasto= 23.75
+    dinero= 100
+
+    Vuelto= dinero-gasto
+
+    Pesos=int(Vuelto)
+    Centavos=int((Vuelto-Pesos)*100)
+
+    print("\nVuelto\n")
+    print(f"Pesos:\n{Pesos}")
+    print(f"Centavos:\n{Centavos}")
