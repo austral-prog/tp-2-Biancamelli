bc98faa1365ccd8fe57ca40be383a72b6cac77d5
Biancamelli <biancamelli249@gmail.com>
2024-08-13 17:23:17 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
-def earth():
+ def earth():
     x = "Bangladesh"
     y = "Barbados"
+    f1= f"The result of {x} comes first in the dictionary than {y} is {(x<y)}"
+    f2= F"The result of {y} comes first in the dictionary than {x} is {(x>y)}"
+    print(str(f1))
+    print(str(f2))
+
+
